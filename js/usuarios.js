b71e3c0138134e735729344a5cdb38f514633ff7
JoaquinGonzalezZapiola <joaquingz@outlook.com>
2022-12-06 22:17:47 -0300

    actualización css js y html

---
+++ js/usuarios.js
@@ -0,0 +1,14 @@
+
+const urlUsuarios = "https://jsonplaceholder.typicode.com/users";
+const listaUsuarios = document.querySelector("#listaUsuarios")
+
+
+fetch(urlUsuarios)
+  .then(response => response.json())
+  .then((data) => {
+    data.forEach(usuario => {
+        const li = document.createElement("li");
+        li.textContent = usuario.name
+        listaUsuarios.append(li)
+    })
+  })
